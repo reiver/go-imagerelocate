e9cd855734583924142135d4f1b60491726d782b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-19 14:48:00 -0700

    initial commits

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package imagerelocate provides tools to relocate an image.Image to a new location;
+this package works seamlessly with Go's built-in "image", "image/color", and "image/draw" packages.
+*/
+package imagerelocate
